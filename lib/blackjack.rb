14269c008dc9ceae12e1500a42dc627fbd186e0d
Caroline Lee <carolinel2610@gmail.com>
2018-01-10 06:31:43 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,7 +50,7 @@ def invalid_command
   # code invalid_command here
   puts "Please enter a valid command"
 end
-
+end
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
