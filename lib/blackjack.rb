a8984b567eb80c1962bda182c0fef70d92a2a823
Caroline Lee <carolinel2610@gmail.com>
2018-01-10 06:39:20 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,7 +33,8 @@ end
 def initial_round
   # code #initial_round here
   total = deal_card + deal_card
-  display_card_total (total)
+  display_card_total(total)
+  total
 end
 
 def hit? (card_total)
